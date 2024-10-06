d0fc70664c2377bb1f447c73e25de26dd9c7ed13
Laurel Murphy <laureljasper@gmail.com>
2024-10-05 19:59:41 -0700

    Update styling for cards

--- app/components/RecipeDetail.tsx
+++ app/components/RecipeDetail.tsx
@@ -12,8 +12,8 @@ export function RecipeDetail({
       className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50"
       onClick={onClose}
     >
-      <div
-        className="bg-white p-6 rounded-lg max-w-3xl w-full h-auto relative overflow-auto"
+      {/* <div
+        className="bg-white p-6 rounded-lg max-w-4xl w-[100vh] max-h-[500px] relative overflow-auto"
         onClick={(e) => e.stopPropagation()}
       >
         <button
@@ -32,6 +32,32 @@ export function RecipeDetail({
         />
         <h3 className="text-xl font-semibold mb-2">Instructions:</h3>
         <p className="text-base leading-6">{meal.strInstructions}</p>
+      </div> */}
+      <div
+        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+        onClick={onClose}
+      >
+        <div
+          className="bg-white p-6 rounded-lg max-w-4xl w-full max-h-[500px] overflow-auto relative"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <button
+            onClick={onClose}
+            className="absolute top-3 right-3 bg-red-500 text-white py-1 px-3 rounded-lg hover:bg-red-600"
+          >
+            Close
+          </button>
+          <h2 className="text-2xl font-semibold text-center mb-4">
+            {meal.strMeal}
+          </h2>
+          <img
+            src={meal.strMealThumb}
+            alt={meal.strMeal}
+            className="w-1/2 h-auto object-cover mx-auto mb-4 rounded-lg"
+          />
+          <h3 className="text-xl font-semibold mb-2">Instructions:</h3>
+          <p className="text-base leading-6">{meal.strInstructions}</p>
+        </div>
       </div>
     </div>
   );
